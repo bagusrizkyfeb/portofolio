01588c8dd78582207b3ebddbf9e7c3a02573d2c4
bagusrizkyfeb <bagusrizkyfebriy@gmail.com>
2022-05-02 14:19:51 +0700

    update

--- script.js
+++ script.js
@@ -36,23 +36,6 @@ function topFunction() {
 }
 
 
-function reveal() {
-  var reveals = document.querySelectorAll(".about");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
 
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
